960640567bdc3b7c77742c5ebaf11cecf64a97a9
motokikando <kando813motoki@gmail.com>
2022-10-20 13:06:58 +0900

    Add big5 likert scale

--- demo/likert.js
+++ demo/likert.js
@@ -86,29 +86,81 @@ const likert_e = {
   button_label: '次へ'
 };
 
-const e_likert = {
+const big5_likert = {
   type: jsPsychSurvey,
   pages: [
     [
-    createLikert('人と話すのがとても楽しい', 'e1', 'N'),
-    createLikert('大勢の人と一緒にいるのが好きだ。', 'e2', 'N'),
-    createLikert('何かする場合は一人でやる方が好きだ。', 'e3', 'N'),
-    createLikert('人の先頭に立つよりも、むしろ我が道を行く方が良い。', 'e4', 'R'),
-    createLikert('元気があふれて、じっとしていられないことがよくある。', 'e5', 'N'),
-    createLikert('私はいつも何かしている。', 'e6', 'N'),
-    createLikert('非常に活発的な人間だ。', 'e7', 'N'),
-    createLikert('活気のある所にいるのが好きだ。', 'e8', 'N'),
-    createLikert('楽天家ではない。', 'e9', 'R'),
-    createLikert('特にほがらかな人間ではない。', 'e10', 'R'),
-    createLikert('元気で、はつらつとした人間だ。', 'e11', 'N'),
-    createLikert('私はすぐに笑う', 'e12', 'N'),
+      createLikert('人と話すのがとても楽しい', 'e1', 'N'),
+      createLikert('大勢の人と一緒にいるのが好きだ。', 'e2', 'N'),
+      createLikert('何かする場合は一人でやる方が好きだ。', 'e3', 'N'),
+      createLikert('人の先頭に立つよりも、むしろ我が道を行く方が良い。', 'e4', 'R'),
+      createLikert('元気があふれて、じっとしていられないことがよくある。', 'e5', 'N'),
+      createLikert('私はいつも何かしている。', 'e6', 'N'),
+      createLikert('非常に活発的な人間だ。', 'e7', 'N'),
+      createLikert('活気のある所にいるのが好きだ。', 'e8', 'N'),
+      createLikert('楽天家ではない。', 'e9', 'R'),
+      createLikert('特にほがらかな人間ではない。', 'e10', 'R'),
+      createLikert('元気で、はつらつとした人間だ。', 'e11', 'N'),
+      createLikert('私はすぐに笑う', 'e12', 'N'),
     ],
+    [
+      createLikert('私は心配性ではない。', 'n1', 'R'),
+      createLikert('恐ろしいとか、不安だとか感じることはめったにない。', 'n2', 'R'),
+      createLikert('よく緊張したり神経過敏になったりする。', 'n3', 'N'),
+      createLikert('人の仕打ちによく腹を立てる。', 'n4', 'N'),
+      createLikert('さびしくなったり、憂うつになったりすることはめったにない。', 'n5', 'R'),
+      createLikert('自分はまったく価値がないと感じることが時々ある。', 'n6', 'N'),
+      createLikert('悲しくなったり、落ち込んだりすることはほとんどない。', 'n7', 'N'),
+      createLikert('物事がうまくいかないと、がっかりして、あきらめたくなることがしょっちゅうある。', 'n8', 'N'),
+      createLikert('穴があったら入りたいと思うほど、恥ずかしいことがたまにある。', 'n9', 'N'),
+      createLikert('劣等感を持つことがよくある。', 'n10', 'N'),
+      createLikert('どうしようもなくて、その問題を誰かに解決してもらいたいと思うことがよくある。', 'n11', 'N'),
+      createLikert('ストレスが多いと、自分が「めちゃくちゃ」になるように感じることもある。', 'n12', 'N'),
+    ],
+    [
+      createLikert('どんなことでも現実的に考え、空想や夢のような考えを避けるように努めている。', 'o1', 'R'),
+      createLikert('空想にふけって時間を無駄にするのは好きではない。', 'o2', 'N'),
+      createLikert('芸術作品や自然の中で見つけた形に興味をひかれる。', 'o3', 'N'),
+      createLikert('詩を読んでもなにも感じない。', 'o4', 'R'),
+      createLikert('場所が変わると気分や気持ちが変わるものだが、私は気分が変わったとは感じない。', 'o5', 'R'),
+      createLikert('いったんうまくいくと思ったら、あくまでもそのやり方を変えない。', 'o6', 'R'),
+      createLikert('新しい、珍しい食べ物を試してみることがよくある。', 'o7', 'N'),
+      createLikert('抽象的な考えや理論を楽しむことがよくある。', 'o8', 'N'),
+      createLikert('宇宙の本質や、人類が置かれている状態に思いをめぐらすことにはほとんど興味がない。', 'o9', 'R'),
+      createLikert('知的好奇心が強い。', 'o10', 'N'),
+      createLikert('学生に対しては、いろいろな意見や考え方があることを教えるのではなく、一つの考え方を教えるだけで十分だ。', 'o11', 'R'),
+      createLikert('道徳的な判断は、昔からある基準に基づくべきだ。', 'o12', 'R'),
+    ],
+    [
+      createLikert('持ち物をきちんとし、きれいにしている。', 'c1', 'N'),
+      createLikert('几帳面ではない。', 'c2', 'R'),
+      createLikert('人は私のことを、きちんとした人間だと思っていない。', 'c3', 'R'),
+      createLikert('割り当てられた仕事を、すべてきちんとやるよう努めている。', 'c4', 'N'),
+      createLikert('人の期待にそったり、約束を守ったりしなければならないのに、時々そうではない。', 'c5', 'R'),
+      createLikert('一人でやるゲームでは、時々ずるをする。', 'c6', 'R'),
+      createLikert('明確な目標を持っており、それに向かって整然としたやり方で取り組んでいる。', 'c7', 'N'),
+      createLikert('自分の目標を達成するようにがんばる。', 'c8', 'N'),
+      createLikert('やることすべてにおいて、志を高く持ってがんばる。', 'c9', 'N'),
+      createLikert('時間どおりに物事をやり終えるよう、自分のペースを守るのが得意だ。', 'c10', 'N'),
+      createLikert('身を入れて仕事を始めるまでに、時間がかかる。', 'c11', 'R'),
+      createLikert('バリバリと仕事をやって、それをやり遂げる。', 'c12', 'N'),
+    ],
+    [
+      createLikert('人の考えを皮肉っぽく疑いの目で見がちだ。', 'a1', 'R'),
+      createLikert('ほうっておけば、たいていの人は私を出し抜こうとするだろう。', 'a2', 'R'),
+      createLikert('自分の望むものを手に入れるためなら、人を操ることもためらわない。', 'a3', 'R'),
+      createLikert('私のことを、自分勝手で自分のことしか考えない人間だと思っている人がいる。', 'a4', 'R'),
+      createLikert('私は誰にでも好意を持って接しようとする。', 'a5', 'N'),
+      createLikert('私を冷たく計算高いと見ている人がいる。', 'a6', 'R'),
+      createLikert('私はいつも他の人を思いやる人間であろうとしている。', 'a7', 'N'),
+      createLikert('私はほとんどの人から好かれている。 ', 'a8', 'N'),
+      createLikert('人と張り合うよりも協力しあう方が好きだ。', 'a9', 'N'),
+      createLikert('嫌いな人には、そう知らせてやる。', 'a10', 'R'),
+      createLikert('家族や同僚とよく口論をする。', 'a11', 'R'),
+      createLikert('私は現実的で、情では動かない。', 'a12', 'R'),
+    ]
   ],
   button_label_next: '次へ',
   button_label_back: '戻る',
   button_label_finish: '終了'
-}
-
-const big5_likert = {
-  timeline: [e_likert]
 }
